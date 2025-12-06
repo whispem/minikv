bcd914d09d07647a513bf9456fcfc58d90408e77
Em <contact.whispem@gmail.com>
2025-12-06 16:15:01 +0100

    Implement initial gRPC service for Coordinator

---
+++ src/coordinator/grpc.rs
@@ -0,0 +1,50 @@
+//! Coordinator gRPC service (internal)
+
+use crate::proto::coordinator_internal_server::{CoordinatorInternal, CoordinatorInternalServer};
+use crate::proto::*;
+use tonic::{Request, Response, Status};
+
+pub struct CoordGrpcService {}
+
+impl CoordGrpcService {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn into_server(self) -> CoordinatorInternalServer<Self> {
+        CoordinatorInternalServer::new(self)
+    }
+}
+
+#[tonic::async_trait]
+impl CoordinatorInternal for CoordGrpcService {
+    async fn request_vote(&self, _req: Request<VoteRequest>) -> Result<Response<VoteResponse>, Status> {
+        // TODO: Implement Raft RequestVote RPC
+        Err(Status::unimplemented("RequestVote not implemented"))
+    }
+
+    async fn append_entries(&self, _req: Request<AppendRequest>) -> Result<Response<AppendResponse>, Status> {
+        // TODO: Implement Raft AppendEntries RPC
+        Err(Status::unimplemented("AppendEntries not implemented"))
+    }
+
+    async fn install_snapshot(&self, _req: Request<SnapshotRequest>) -> Result<Response<SnapshotResponse>, Status> {
+        Err(Status::unimplemented("InstallSnapshot not implemented"))
+    }
+
+    async fn join(&self, _req: Request<JoinRequest>) -> Result<Response<JoinResponse>, Status> {
+        // TODO: Handle volume registration
+        Ok(Response::new(JoinResponse {
+            ok: true,
+            cluster_id: "cluster-1".to_string(),
+        }))
+    }
+
+    async fn heartbeat(&self, _req: Request<HeartbeatRequest>) -> Result<Response<HeartbeatResponse>, Status> {
+        // TODO: Update volume state
+        Ok(Response::new(HeartbeatResponse {
+            ok: true,
+            commands: vec![],
+        }))
+    }
+}
