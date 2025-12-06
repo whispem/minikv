984c64a2292cb9ca370c4b1995b9345800dc1013
Em <contact.whispem@gmail.com>
2025-12-06 22:11:17 +0100

    Add new functions and types to common module

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -7,5 +7,5 @@ pub mod utils;
 
 pub use config::{Config, CoordinatorConfig, NodeRole, RuntimeConfig, VolumeConfig, WalSyncPolicy};
 pub use error::{Error, Result};
-pub use hash::{blake3_hash, hrw_hash, shard_key};
-pub use utils::{decode_key, encode_key, format_bytes, parse_duration, timestamp_now, NodeState};
+pub use hash::{blake3_hash, hrw_hash, shard_key, select_replicas, ConsistentHashRing, Blake3Hasher, blob_prefix};
+pub use utils::{decode_key, encode_key, format_bytes, parse_duration, timestamp_now, NodeState, crc32};
