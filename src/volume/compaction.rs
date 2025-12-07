cf22ac664ba1f681e24c82d73f41d2c14fac362e
Em <contact.whispem@gmail.com>
2025-12-07 10:40:06 +0100

    Refactor compact_store function to simplify return

--- src/volume/compaction.rs
+++ src/volume/compaction.rs
@@ -1,7 +1,7 @@
 use crate::volume::blob::BlobStore;
+use crate::common::Result;
 use std::sync::MutexGuard;
 
-pub fn compact_store(store: &mut MutexGuard<'_, BlobStore>) -> Result<(), String> {
-    store.compact()?;
-    Ok(())
+pub fn compact_store(store: &mut MutexGuard<'_, BlobStore>) -> Result<()> {
+    store.compact()
 }
