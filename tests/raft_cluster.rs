0607ad5ae337fbc9d5564de5f06848f1a7d808c0
Em <contact.whispem@gmail.com>
2025-12-30 21:52:43 +0100

    Update comments for clarity in raft_cluster tests

--- tests/raft_cluster.rs
+++ tests/raft_cluster.rs
@@ -10,10 +10,10 @@ async fn raft_election_and_replication() {
     let node1 = Arc::new(RaftNode::new("node1".to_string()));
     let node2 = Arc::new(RaftNode::new("node2".to_string()));
     let node3 = Arc::new(RaftNode::new("node3".to_string()));
-    // peers inutilisé dans ce test
+    // peers unused in this test
 
     // Start an election on node1
-    // Force node1 à devenir leader pour le test local
+    // Force node1 to become leader for local test
     node1.become_leader();
     assert!(node1.is_leader());
     assert_eq!(node1.get_role(), RaftRole::Leader);
@@ -44,7 +44,7 @@ async fn raft_election_and_replication() {
 
     // Simulate failover: leader steps down, node2 starts an election
     node1.step_down(node1.get_term() + 1, Some("node2".to_string()));
-    // Force node2 à devenir leader pour le test local
+    // Force node2 to become leader for local test
     node2.become_leader();
     assert!(node2.is_leader());
     assert_eq!(node2.get_role(), RaftRole::Leader);
